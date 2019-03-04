79e532deefa6c3a4a05f1baf94c1182e6e1a032e
Iacob232 <32065189+Iacob232@users.noreply.github.com>
2019-03-04 10:32:10 -0500

    Telemarketer

--- src/Telemarketer.java
+++ src/Telemarketer.java
@@ -6,20 +6,31 @@ public class Telemarketer {
 		/*
 		 * Follow the instructions in JuniorProblem.pdf
 		 * Use the following values to test your program and record your results
-		 * 1) 8,6,6,7
-		 * 2) 7,6,6,8
-		 * 3) 9,5,6,9
-		 * 4) 3,4,5,6
-		 * 5) 0,8,8,0
-		 * 6) 8,5,5,9
-		 * 7) 9,1,1,9
-		 * 8) 9,9,9,9
-		 * 9) 9,1,2,8
-		 * 10) 8,7,4,8 
-		 * 11) 8,2,2,7
-		 * 12) 1,3,3,9
-		 * 13) 1,0,2,6
+		 * 1) 8,6,6,7 answer
+		 * 2) 7,6,6,8 answer
+		 * 3) 9,5,6,9 answer
+		 * 4) 3,4,5,6 answer
+		 * 5) 0,8,8,0 answer
+		 * 6) 8,5,5,9 ignore
+		 * 7) 9,1,1,9 ignore
+		 * 8) 9,9,9,9 ignore
+		 * 9) 9,1,2,8 answer
+		 * 10) 8,7,4,8 answer 
+		 * 11) 8,2,2,7 answer
+		 * 12) 1,3,3,9 answer
+		 * 13) 1,0,2,6 answer
 		 */
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int c=in.nextInt();
+		int d=in.nextInt();
+		
+		if ((a==9||a==8)&&(d==9||d==8)&&(b==c)){
+			System.out.println("ignore");
+		}else{
+			System.out.println("answer");
+		}
 	}
 
 }
